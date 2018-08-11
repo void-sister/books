8c4123b571f86513cb7e024beaccd8228c7a1d77
Varya <el.varvara.el@gmail.com>
2018-08-11 14:51:07 +0300

    Add fields name and book_id

--- database/migrations/2018_08_11_113649_create_authors_table.php
+++ database/migrations/2018_08_11_113649_create_authors_table.php
@@ -15,6 +15,8 @@ class CreateAuthorsTable extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->integer('book_id');
             $table->timestamps();
         });
     }
