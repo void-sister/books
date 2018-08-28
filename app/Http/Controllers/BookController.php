991cca8ea44289c0bb313b5956d0534b3842f014
void-sister <el.varvara.el@gmail.com>
2018-08-28 23:16:58 +0300

    Add destroy method for BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -88,6 +88,9 @@ class BookController extends Controller
      */
     public function destroy($id)
     {
-        //
+      $book = Book::findOrFail($id);
+      $book->delete();
+
+      return redirect('books');
     }
 }
