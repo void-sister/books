82ecde87b34128d0c919a66e0f054b806e778cc4
void-sister <el.varvara.el@gmail.com>
2018-08-28 03:03:36 +0300

    Add create blade for BookController

---
+++ resources/views/books/create.blade.php
@@ -0,0 +1,29 @@
+@extends('app')
+
+@section('title', 'Create Book')
+
+@section('h2', 'Create Book')
+
+@section('content')
+
+
+<form method="POST" action="{{ action('BookController@store') }}">
+  {{ csrf_field() }}
+  <div>
+    <label for="author_id">Choose Author</label>
+    <select name="author_id" id="author_id">
+      @foreach ($authors as $author)
+        <option value="{{ $author->id }}">{{ $author->name }}</option>
+      @endforeach
+    </select>
+  </div>
+
+  <div>
+    <label for="title">Book Title</label>
+    <input type="text" id="title" name="title" value="{{ old('title') }}">
+  </div>
+  <button type="submit" class="btn btn-primary">Create</button>
+</form>
+
+
+@endsection
